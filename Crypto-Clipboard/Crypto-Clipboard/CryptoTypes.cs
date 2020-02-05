0fefab88e8ea54fbf466594a80cf147b6a90b1fb
Pablo Bonilla <pbonilla29@hotmail.com>
2020-02-04 20:15:06 -0600

    Add user defined cryptocurrency addresses

---
+++ Crypto-Clipboard/Crypto-Clipboard/CryptoTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto_Clipboard
+{
+    class CryptoTypes
+    {
+        public static readonly string BTC = "";
+        public static readonly string ETHEREUM = "";
+
+        public CryptoTypes() { }
+    }
+}
